c5b6349f56636b6b9209a7cc8503d7b7a7ac52b8
marcus1337 <blurgas@hotmail.com>
2023-01-05 14:39:20 +0100

    checkers

---
+++ src/board/mod.rs
@@ -0,0 +1,26 @@
+
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum GameResult {
+    OneWin,
+    TwoWin,
+    Draw,
+    OnGoing,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+pub struct Board{
+
+}
+
+impl Board{
+
+    pub fn new() -> Self {
+        Self {
+            
+        }
+    }
+
+}
---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+
+extern crate libc;
+pub mod board;
+pub mod ai;
+use self::board::Board;
+
+#[repr(C)]
+pub struct Checkers {
+    board: Board,
+}
+
+impl Checkers{
+
+    #[no_mangle]
+    pub extern "C" fn checkers_make() -> Self {
+        Self {
+            board: Board::new(),
+        }
+    }
+
+}
+
